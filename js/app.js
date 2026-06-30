604ec1cb0f95b2a485d1e92c05c4dfcb7c648be1
Maxim <maxtab1@mail.ru>
2026-06-30 17:13:06 +0300

    regular

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-import "@maxmov/css/reset.css";
+const sliderRed = document.getElementById("slider_red_id");
+const sliderGreen = document.getElementById("slider_green_id");
+const sliderBlue = document.getElementById("slider_blue_id");
+const valueRed = document.getElementById("value_red_id");
+const valueGreen = document.getElementById("value_green_id");
+const valueBlue = document.getElementById("value_blue_id");
+const btn = document.getElementById("btn_id");
+//
+function updateColor() {
+	const r = sliderRed.value;
+	const g = sliderGreen.value;
+	const b = sliderBlue.value;
+	//
+	valueRed.textContent = r;
+	valueGreen.textContent = g;
+	valueBlue.textContent = b;
+	//
+	btn.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+//
+sliderRed.addEventListener("input", updateColor);
+sliderGreen.addEventListener("input", updateColor);
+sliderBlue.addEventListener("input", updateColor);
+//
+updateColor();
